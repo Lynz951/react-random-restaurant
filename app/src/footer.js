221391f4955f395716f432c63a0b02984645faed
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-10-13 18:43:53 +0000

    footer ok

--- app/src/footer.js
+++ app/src/footer.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
+import map from './map.png'
 
 
 
     const Footer = () => (
       <div className="footer">
-        <p>This is some content in sticky footer</p>
+        <div className="address">
+        <b><h2>Wise Norah's is located at:</h2> </b>
+            <p>348 E. Main St.
+            Lexington, KY 40508</p>
+            <img src={map} alt="map" className="map"/>
+            </div>
+        <div className="hours">
+            <h2>Hours of Operation</h2>
+            <h6>M-F: 9am to 10pm
+                Saturday: 8am to 11pm
+                Sunday: 11am to 10pm
+            </h6>
+        </div>
       </div>
     );
     
