65c4c62fc91a59b38e915dc32a2f227f730307e9
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-10-16 20:52:26 +0000

    menu shows up

--- app/src/getmenu.js
+++ app/src/getmenu.js
@@ -9,7 +9,7 @@ function GetMenu() {
     const [page, setPage] = useState('home')
     const [name , setName] = useState()
     const [data, setData] = useState([])
-
+    
     useEffect(() => {
         axios.get('https://astute-baton-362318.ue.r.appspot.com/api/json/')
             .then((response) => {
@@ -20,17 +20,46 @@ function GetMenu() {
         if(data.length === 0) {
             return null
         }
-        
-        console.log(data)
-        console.log(data[28].id)
+
+    
         function BtnClick() {
-            setName(data[0].category.title)
-            
+            setName(data[10].category.title)   
         }
+        console.log(data);
+        const apps = (
+            <div className='menu'>
+                <h1>Appetizers</h1>
+                <ul>
+                    {data.filter(item => [17, 32, 45, 83, 66, 80].includes(item.id)).map(item => 
+                        <li key={item.id}>
+                        {item.title}{"    $"}{item.price}
+                        </li>
+                     )}
+                </ul>
+                <h1>Entrees</h1>
+                <ul>
+                    {data.filter(item => [21, 28, 68, 86, 89, 64, 54].includes(item.id)).map(item => 
+                        <li key={item.id}>
+                        {item.title}{"    $"}{item.price}
+                        </li>
+                     )}
+                </ul>
+                <h1>Desserts</h1>
+                <ul>
+                    {data.filter(item => [5, 22, 29, 85, 91].includes(item.id)).map(item => 
+                        <li key={item.id}>
+                        {item.title}{"    $"}{item.price}
+                        </li>
+                     )}
+                </ul>
+            </div>
+        );
+
         if (page == 'home') {
             return (
           <div className='middle'>
             <h1>{name}</h1>
+            {apps}
             <button onClick={BtnClick}>Get Full Menu</button>
           </div>
         )
